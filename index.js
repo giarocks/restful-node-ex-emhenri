755ce84a4a7fcc8b01cb0f81159a217a5f933894
giarocks <giarocksfab@gmail.com>
2018-02-22 18:39:04 +0530

    Updated Mongodb connection to mongoose

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ const app = express();
 const PORT = 3001;
 
 //mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/demodb',{
-    //useMongoClient:true //not doing this gives err
+//mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/demodb',{
+//     //useMongoClient:true //not doing this gives err
+// });
+
+//mongoose.connect('mongodb://gia:mLabPassword1@ds145438.mlab.com:45438/restful-node-ex-emhenri',{
+         //useMongoClient:true //not doing this gives err
+    
+ //});
+
+ mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://giarocksfab:giarocksfab@ds145438.mlab.com:45438/restful-node-ex-emhenri'); 
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("connection sucess");
 });
+
+
+//mongoose.connect('mongodb://gomobileuser:gomobile123@ds117148.mlab.com:17148/gomobile'); 
+var db = mongoose.connection;
+//mongodb://<dbuser>:<dbpassword>@ds145438.mlab.com:45438/restful-node-ex-emhenri
 
 //body parser setup
 app.use(bodyParser.urlencoded({extended:true}))
